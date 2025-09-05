838ed5185e694145551c06c2b33ec14fa8ef0e0f
Josh Ellis <juiceellish@gmail.com>
2025-09-04 20:34:02 -0500

    Try reouter refresh on email update

--- components/Header.jsx
+++ components/Header.jsx
@@ -207,7 +207,7 @@ export default function Header({ difficulty, onDifficultyChange }) {
             setUpdateEmailAlertMessage("Your email has been updated!");
             await new Promise(resolve => setTimeout(resolve, 3000));
             setUpdateEmailAlertMessage('');
-            router.refresh();
+            router.replace('/');
         } catch (err) {
             console.error('There was a problem updating the email:', err);
             setUpdateEmailLoading(false);
